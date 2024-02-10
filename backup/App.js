9478cbce1112eeaddbd10c24049310320ec34357
Zahar <zahratulnazirah@gmail.com>
2024-02-11 00:14:10 +0800

    initial commit
    my first lesson on creating react app

---
+++ backup/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+return <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus voluptate, sed exercitationem doloremque ullam aspernatur inventore dignissimos maiores nemo ipsam eligendi natus dolore. Quis eaque voluptatibus nulla debitis neque omnis aut expedita harum eos quae ipsa eveniet, repudiandae quibusdam dicta ratione illum beatae. Soluta impedit dicta consequatur dolorem, labore eaque harum quisquam quos a debitis explicabo! Fugiat, corporis. Porro maiores beatae ipsa impedit nihil. Laborum, debitis. Veritatis doloremque obcaecati a vero ad praesentium optio asperiores voluptate eaque dolore? Quam fuga asperiores ipsam ex dolores unde, optio repudiandae atque nam quasi dolor quos id ducimus vel porro ea sed aspernatur adipisci.</p>
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+let element = (
+  <div className="container">
+    <div classNameName="row text-center">
+      <strong>hellow world</strong>
+    </div>
+    <div classNameName="row">
+      <div classNameName="col-12 text-center">
+        <button className="btn btn-success">click me!</button>
+      </div>
+    </div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<NavBar />, document.getElementById("root"));
