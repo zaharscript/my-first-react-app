f160667d783e879c5c398b8fe0c88302ab5e9214
Zahar <zahratulnazirah@gmail.com>
2024-02-12 22:51:24 +0800

    adding array data
    adding  array for dynamic data in table

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -3,13 +3,43 @@ import React, { Component } from "react";
 export default class MainComponent extends Component {
   state = {
     pageTitle: "Customers",
-    count: 5,
+    count: 0,
     customers: [
-      { id: 1, name: "Zahra", Phone: "123-234" },
-      { id: 2, name: "Annas", Phone: "143-254" },
-      { id: 3, name: "Baihaqi", Phone: "363-876" },
-      { id: 4, name: "Hadi", Phone: "122-267" },
-      { id: 5, name: "Muhaimin", Phone: "101-839" },
+      {
+        id: 1,
+        name: "Zahra",
+        Country: "Malaysia",
+        Phone: "123-234",
+        address: { city: "Kajang" },
+      },
+      {
+        id: 2,
+        name: "Annas",
+        country: "German",
+        Phone: "143-254",
+        address: { city: "Ipoh" },
+      },
+      {
+        id: 3,
+        name: "Baihaqi",
+        country: "United Kingdom",
+        Phone: "363-876",
+        address: { city: "Bertam" },
+      },
+      {
+        id: 4,
+        name: "Hadi",
+        country: "Indonesia",
+        Phone: "122-267",
+        address: { city: "Bayan lepas" },
+      },
+      {
+        id: 5,
+        name: "Muhaimin",
+        country: "Australia",
+        Phone: "101-839",
+        address: { city: "Johor Baharu" },
+      },
     ],
   };
   render() {
@@ -21,30 +51,31 @@ export default class MainComponent extends Component {
         </h3>
         <button
           type="button"
-          class="btn btn-warning"
+          className="btn btn-warning"
           onClick={this.incrementCount}
         >
           Add Count++
         </button>
 
-        <table class="table ">
+        <table className="table ">
           <thead>
             <tr>
               <th scope="col">#</th>
               <th scope="col">customer name</th>
-              <th scope="col">City</th>
+              <th scope="col">Country</th>
               <th scope="col">Phone</th>
+              <th scope="col">City</th>
             </tr>
           </thead>
           <tbody>
-            {this.state.customers.map((cust) => {
+            {this.state.customers.map((customer) => {
               return (
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                <tr key="{customer.id}">
+                  <td>{customer.id}</td>
+                  <td>{customer.name}</td>
+                  <td>{customer.country}</td>
+                  <td>{customer.Phone}</td>
+                  <td>{customer.address.city}</td>
                 </tr>
               );
             })}
@@ -54,15 +85,16 @@ export default class MainComponent extends Component {
     );
   }
   //execute when user click the button
-  incrementCount = () => {
-    this.setState({
-      count: 10,
-    });
-  };
-  // increment function
   // incrementCount = () => {
   //   this.setState({
-  // count: this.state.count + 1
-  // });
+  //     count: 10,
+  //   });
   // };
+
+  // increment function
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
 }
